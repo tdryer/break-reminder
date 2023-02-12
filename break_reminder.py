7daf12e83d6398ef4cc50986c78fab8e06973264
Tom Dryer <tomdryer.com@gmail.com>
2023-02-11 22:51:25 -0800

    Refactor into class

--- break_reminder.py
+++ break_reminder.py
@@ -11,8 +11,7 @@ from gi.repository import Notify, GnomeDesktop, GLib
 LOGGER = logging.getLogger(__name__)
 
 
-# TODO: use these
-BREAK_INTERVAL = datetime.timedelta(hours=1)
+BREAK_INTERVAL = datetime.timedelta(minutes=60)
 BREAK_DURATION = datetime.timedelta(minutes=5)
 IDLE_TIMEOUT = datetime.timedelta(minutes=1)
 
@@ -31,7 +30,6 @@ class Timer:
             self._callback()
         except:
             LOGGER.exception("Exception in timer callback")
-        self.reset()
         return False
 
     def _remove_source(self):
@@ -58,47 +56,76 @@ class Timer:
         self._source = GLib.timeout_add(self._interval, self._on_timeout_expiry)
 
 
+class BreakReminder:
+    def __init__(self, idle_monitor):
+        self._idle_monitor = idle_monitor
+        self._timer = None
+
+    def start(self):
+        self._start_break_timer()
+        idle_watch_id = self._idle_monitor.add_idle_watch(
+            IDLE_TIMEOUT / datetime.timedelta(milliseconds=1), self._on_idle_start
+        )
+
+    def _start_break_timer(self):
+        """Start timer for the next break."""
+        interval_ms = (BREAK_INTERVAL - BREAK_DURATION) / datetime.timedelta(
+            milliseconds=1
+        )
+        self._timer = Timer(interval_ms, self._on_start_break)
+
+    def _on_start_break(self):
+        """Callback when break is started."""
+        logging.info("Start break")
+        # TODO: Need to track length of break so idle time does not delay next break
+        notification = Notify.Notification.new("Break Time")
+        notification.set_urgency(Notify.Urgency.CRITICAL)
+        notification.show()
+
+        GLib.timeout_add(
+            BREAK_DURATION / datetime.timedelta(milliseconds=1),
+            self._on_finish_break,
+            notification,
+        )
+
+    def _on_finish_break(self, notification):
+        """Callback when break is finished."""
+        logging.info("Finish break")
+        notification.close()
+        # TODO: Pause the timer if currently idle
+        self._start_break_timer()
+
+    def _on_idle_start(self, monitor, watch_id):
+        """Callback when idle is started."""
+        LOGGER.info("Idle start")
+        # self._timer.pause()
+        idle_timestamp = GLib.get_monotonic_time()
+        user_active_watch_id = self._idle_monitor.add_user_active_watch(
+            self._on_idle_end, idle_timestamp
+        )
+
+    def _on_idle_end(self, monitor, watch_id, idle_timestamp):
+        """Callback when idle is finished."""
+        LOGGER.info("Idle end")
+        # TODO: Reset timer instead if too much time elapsed
+        # self._timer.resume()
+        elapsed = GLib.get_monotonic_time() - idle_timestamp
+        # LOGGER.info("User is active again after %s", elapsed // 1000)
+
+
 def main():
     logging.basicConfig(
         format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO
     )
 
     if not Notify.init("Break Reminder"):
-        sys.exit("Failed to initialize desktop notifications")
+        sys.exit("Failed to initialize notifier")
 
-    # TODO:
-    def on_finish_break():
-        logging.info("Finished break")
-
-    def on_start_break():
-        logging.info("Showing notification")
-        # TODO: Should probably replace previous notification
-        # TODO: Need to track length of break so idle time does not delay next break
-        notification = Notify.Notification.new("Break Time")
-        notification.set_urgency(Notify.Urgency.CRITICAL)
-        notification.show()
-
-    timer = Timer(5000, on_start_break)
-
-    # stops working if idle_monitor is GC'd
     idle_monitor = GnomeDesktop.IdleMonitor()
-    assert idle_monitor.init()
+    if not idle_monitor.init():
+        sys.exit("Failed to initialize idle monitor")
 
-    def active_cb(monitor, watch_id, idle_timestamp):
-        # TODO: Reset timer instead if too much time elapsed
-        timer.resume()
-        elapsed = GLib.get_monotonic_time() - idle_timestamp
-        LOGGER.info("User is active again after %s", elapsed // 1000)
-
-    def idle_cb(monitor, watch_id):
-        LOGGER.info("User is idle")
-        timer.pause()
-        idle_timestamp = GLib.get_monotonic_time()
-        user_active_watch_id = idle_monitor.add_user_active_watch(
-            active_cb, idle_timestamp
-        )
-
-    idle_watch_id = idle_monitor.add_idle_watch(1000, idle_cb)
+    BreakReminder(idle_monitor).start()
 
     try:
         GLib.MainLoop().run()
